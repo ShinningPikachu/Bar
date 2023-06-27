7edfbe3738861aa64c39dcf9f50fecb67803b6ab
Mona Lisa <jinhengl2001@gmail.com>
2023-06-27 22:23:06 +0200

    interface distributions

--- Represent.java
+++ Represent.java
@@ -1,10 +1,10 @@
-import Representacion.MainPanel;
+import Representacion.*;
 
 public class Represent {
     static Plataform PT;
     static MainPanel MP;
     static public void main(String[] arg){
         PT = new Plataform();
-        MP = new MainPanel(PT.GetListProduct());
+        //MP = new MainPanel(PT.GetListProduct());
     }
 }
--- Representacion/MainPanel.java
+++ Representacion/MainPanel.java
@@ -1,57 +1,42 @@
 package Representacion;
+
 import javax.swing.*;
 import java.awt.*;
 import java.util.Set;
 
 public class MainPanel {
-    private JPanel mainPanel;
-    private JPanel panel1;
-    private JPanel panel2;
-    private JPanel panel3;
-    private JFrame frame;
+    private JPanel Main;
+    private JPanel tables;
+    private JPanel terraza;
+    private JPanel down;
+    private JPanel top;
+    private JPanel products;
+    private JPanel type;
+    private JPanel ingredient;
+    private JPanel allproduct;
+    private JPanel ticket;
+    private JPanel ticketBottom;
+    private JPanel ticketzone;
 
-    public MainPanel(Set<String> allProduct){
-        // Create a JFrame to hold the interface components
-        frame = new JFrame("Frame with Different Panel Sizes");
+    public MainPanel(){
+        terraza.setBackground(Color.BLUE);
+        down.setBackground(Color.BLACK);
+        top.setBackground(Color.CYAN);
+
+        type.setBackground(Color.GRAY);
+        ingredient.setBackground(Color.GREEN);
+        allproduct.setBackground(Color.MAGENTA);
+
+        ticket.setBackground(Color.RED);
+        ticketBottom.setBackground(Color.BLUE);
+    }
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Frame with Different Panel Sizes");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH); // Maximize the frame to occupy the entire screen
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-
-        // Create the main panel
-        mainPanel = new JPanel(new BorderLayout());
-
-        // Create sub-panel 1 with specific size
-        panel1 = new JPanel();
-        for (String element : allProduct) {
-            JButton button = new JButton(element);
-            panel1.add(button);
-        }
-        panel1.setBackground(Color.RED);
-        panel1.setPreferredSize(new Dimension(2*screenSize.width/5, screenSize.height));
-        mainPanel.add(panel1, BorderLayout.LINE_START);
-
-        // Create sub-panel 2 with specific size
-        panel2 = new JPanel();
-        for (String element : allProduct) {
-            JButton button = new JButton(element);
-            panel2.add(button);
-        }
-        panel2.setBackground(Color.GREEN);
-        panel2.setPreferredSize(new Dimension(2*screenSize.width/5, screenSize.height));
-        mainPanel.add(panel2, BorderLayout.CENTER);
-
-        // Create sub-panel 3 with specific size
-        panel3 = new JPanel();
-        for (String element : allProduct) {
-            JButton button = new JButton(element);
-            panel3.add(button);
-        }
-        panel3.setBackground(Color.BLUE);
-        panel3.setPreferredSize(new Dimension(2*screenSize.width/5, screenSize.height));
-        mainPanel.add(panel3, BorderLayout.LINE_END);
-
-        frame.setContentPane(mainPanel);
-        frame.setSize(screenSize.width, screenSize.height);
+        frame.setContentPane(new MainPanel().Main);
+        frame.pack();
         frame.setVisible(true);
     }
 }
